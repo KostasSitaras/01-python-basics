20cff4b3a463e6774bcac46c0f50c28bcddcb67a
Konstantinos Sitaras <kostassitaras1@gmail.com>
2026-07-08 16:31:34 +0300

    Add simple calculator

---
+++ calculator.py
@@ -0,0 +1,10 @@
+print   ("Simple Calculator")
+
+first_number = float(input("Enter the First Number: "))
+second_number = float(input("Enter the Second Number: "))
+
+result = first_number + second_number
+
+print("The Result =",result)
+
+
